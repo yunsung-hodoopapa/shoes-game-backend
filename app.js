af3178be56b21a12cd93d2d86d8c51de2b61baa0
yunsungyang-omc <vappet3477@gmail.com>
2021-10-15 11:31:14 +0900

    add bcrypt logic and make token process

--- app.js
+++ app.js
@@ -41,13 +41,57 @@ app.get('/', (req, res) => res.send('hello world!!'));
 app.post('/register', (req, res) => {
   // 회원가입을 할때 필요한 것
   // post로 넘어온 데이터를 받아서 DB에 저장한다.
-  console.log('-----');
-  console.log(req.body);
-  console.log('-----');
   const user = new User(req.body);
+  console.log('-----');
+  console.log(user);
+  console.log('-----');
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({ success: true });
+  });
+});
+
+app.post('/login', (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (err) {
+      return res.json({
+        loginSuccess: false,
+        message: '존재하지 않는 아이디입니다.',
+      });
+    }
+    // console.log(req.body.password);
+    console.log(user);
+    console.log('-----1');
+    console.log(req.body.password);
+    console.log('-----2');
+    user
+      .comparePassword(req.body.password)
+      .then((isMatch) => {
+        console.log('iam hodoo1');
+        if (!isMatch) {
+          res.json({
+            loginSuccess: false,
+            message: '비밀번호가 일치하지 않습니다',
+          });
+        }
+        user
+          .generateToken()
+          .then((user) => {
+            console.log('iam hodoo2');
+            res
+              .cookie('x_auth', user.token)
+              .status(200)
+              .json({ loginSuccess: true, userId: user._id });
+          })
+          .catch((err) => {
+            console.log('iam hodoo3');
+            res.status(400).send(err);
+          });
+      })
+      .catch((err) => {
+        console.log(err, 'iam hodoo4');
+        res.json({ loginSuccess: false, err })
+      });
   });
 });
 
--- schemas/user.js
+++ schemas/user.js
@@ -1,44 +1,44 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+
 // saltRounds는 암호화된 문구의 길이를 의미한다.
 
 const { Schema } = mongoose;
 
-const userSchema = new Schema(
-  {
-    // id: {
-    //   type: String,
-    //   required: true,
-    //   unique: true,
-    // },
-    email: {
-      type: String,
-      trim: true,
-      unique: 1,
-    },
-    password: {
-      type: String,
-      minlength: 5,
-    },
-    name: {
-      type: String,
-      maxlength: 50,
-    },
-    provider: {
-      type: String,
-    },
-    token: {
-      type: String,
-    },
-    tokenExp: {
-      type: Number,
-    },
-    last_login_date: {
-    type: Date,
-    },
+const userSchema = new Schema({
+  // id: {
+  //   type: String,
+  //   required: true,
+  //   unique: true,
+  // },
+  email: {
+    type: String,
+    trim: true,
+    unique: 1,
   },
-);
+  password: {
+    type: String,
+    minlength: 5,
+  },
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  provider: {
+    type: String,
+  },
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+  last_login_date: {
+    type: Date,
+  },
+});
 
 //save 메소드가 실행되기 전에 비밀번호를 암호화하는 로직을 짜야한다.
 userSchema.pre('save', function (next) {
@@ -58,6 +58,22 @@ userSchema.pre('save', function (next) {
     next();
   }
 });
+
+userSchema.methods.comparePassword = function (plainPassword) {
+  return bcrypt
+    .compare(plainPassword, this.password)
+    .then((isMatch) => isMatch)
+    .catch((err) => err);
+};
+
+userSchema.methods.generateToken = function () {
+  //jwt.sign을 이용해서 jwt 토큰을 생성해준다.
+  const token = jwt.sign(this._id.toHexString(), 'secretToken');
+  this.token = token;
+  return this.save()
+    .then((user) => user)
+    .catch((err) => err);
+};
 
 const User = mongoose.model('User', userSchema);
 
