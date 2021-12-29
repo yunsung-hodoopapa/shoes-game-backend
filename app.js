d6e555130f0fdb6d315cd84db3867ff1ca2b040b
yunsung-hodoopapa <vappet3477@gamail.com>
2021-12-30 04:23:47 +0900

    modifying cors option

--- app.js
+++ app.js
@@ -19,19 +19,28 @@ connect();
 // app.use(express.static(path.join(__dirname, 'public')));
 
 // const corsOptions = {
-//   origin: [
-//     'https://shoesgame.app',
-//     'http://localhost:3000'
-//   ],
+//   origin: 'https://shoesgame.app',
 //   credentials: true,
 // };
+const whitelist  = ['http://localhost:3000', 'https://shoesgame.app']
+const corsOptionsDelegate = function (req, callback) {
+  let corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
 
-app.use(cors());
+app.options('*', cors())
+
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/auth', authRouter);
+app.use('/auth', cors(corsOptionsDelegate), authRouter);
 app.use('/shoes', shoesRouter);
 
 app.listen(app.get('port'), () => {
